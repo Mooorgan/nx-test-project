69f064a7f8df4b5851d1aafc98baa446e400b7e7
Shaishav Paudel <morganhouse15@gmail.com>
2024-12-13 12:58:57 +0545

    test(nx):Test service

--- apps/finance/expenses-registration/src/app/app.component.ts
+++ apps/finance/expenses-registration/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ExpensesService} from '@bt-libs/finance/data-access/expenses'
+import { inject,OnInit } from '@angular/core';
 
 
 
@@ -8,6 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(){
+    this.t.sayHi();
+  }
+
+ t=inject(ExpensesService);
   title = 'finance-expenses-registration';
 }
--- libs/finance/data-access/expenses/src/lib/services/expenses.service.ts
+++ libs/finance/data-access/expenses/src/lib/services/expenses.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class ExpensesService {
 
   constructor() { }
+
+  sayHi(){
+    console.log('Hi this is me ');
+  }
 }
